771b49d86ff605ff3beb5eddce94aaa3b7a5c7b9
Joel Barciauskas <barciajo@gmail.com>
2017-08-10 14:55:47 -0400

    WIP - combining react-bootstrap with ag-grid, have an example modal that will be for league settings

--- src/PlayerGrid.jsx
+++ src/PlayerGrid.jsx
@@ -1,61 +1,90 @@
 import React, {Component} from "react";
 import {AgGridReact} from "ag-grid-react";
-import axios from 'axios';
+import {Button, Grid, Row, Col} from "react-bootstrap";
 
 
 export default class extends Component {
     constructor(props) {
         super(props);
 
+        this.onPlayerPriceEntry = this.onPlayerPriceEntry.bind(this);
         this.state = {
-            columnDefs: this.createColumnDefs(),
-            rowData: []
+            quickFilterText: null,
+            columnDefs: this.createColumnDefs()
         }
-        axios.get(`http://localhost:4000/players`)
-        .then(res => {
-          console.log(res)
-          this.setState({
-            columnDefs: this.state.columnDefs,
-            rowData: res.data
-          });
-        });
     }
 
     onGridReady(params) {
         this.gridApi = params.api;
         this.columnApi = params.columnApi;
-
         this.gridApi.sizeColumnsToFit();
+    }
+
+    onQuickFilterText(event) {
+        this.setState({quickFilterText: event.target.value});
+    }
+
+    onPlayerPriceEntry(event) {
+      if(event.column.colId == "purchase_price") {
+        event.data.purchase_price = parseFloat(event.data.purchase_price);
+        localStorage.setItem(event.data.player_id + ".purchase_price", event.data.purchase_price);
+        this.props.onPlayerPurchase(event);
+      }
+    }
+
+    clearSavedPrices(event) {
+      this.props.rowData.forEach((player) => {
+        player.purchase_price = null;
+        localStorage.removeItem(player.player_id + ".purchase_price");
+      });
+      this.setState({rowData: this.props.rowData});
     }
 
     createColumnDefs() {
         return [
-            {headerName: "Player name", field: "name"},
-            {headerName: "Pos", field: "position"},
-            {headerName: "Team", field: "team"},
-            {headerName: "Projected Points", field: "points"},
-            {headerName: "Base Value ($)", field: "base_price"},
+            {headerName: "Player name", field: "name", filter: "text"},
+            {headerName: "Pos", field: "position", filter: "text"},
+            {headerName: "Team", field: "team", filter: "text"},
+            {headerName: "Projected Points", field: "points", filter: "number", cellRenderer: formatNumber, sortingOrder: ['desc','asc']},
+            {headerName: "Base Value ($)", field: "base_price", filter: "number", cellRenderer: formatNumber, sortingOrder: ['desc','asc']},
+            {headerName: "Inf Value ($)", field: "inflated_price", filter: "number", cellRenderer: formatNumber, sortingOrder: ['desc','asc']},
+            {headerName: "Actual ($)", field: "purchase_price", filter: "number", cellRenderer: formatNumber, sortingOrder: ['desc','asc'], editable: true, cellEditor: "text", onCellValueChanged:this.onPlayerPriceEntry}
         ];
     }
 
     render() {
         let containerStyle = {
-            height: 115,
-            width: 500
+            height: "500px",
         };
 
         return (
             <div style={containerStyle} className="ag-fresh">
-                <h1>Players</h1>
+            <h1>Players</h1>
+                <input type="text" onChange={this.onQuickFilterText.bind(this)}
+                                           placeholder="Type text to filter..."/>
+                <Button id="btDestroyGrid" onClick={this.clearSavedPrices}>Clear purchase prices</Button>
                 <AgGridReact
                     // properties
                     columnDefs={this.state.columnDefs}
-                    rowData={this.state.rowData}
+                    rowData={this.props.rowData}
+                    quickFilterText={this.state.quickFilterText}
+
+                    enableSorting
+                    enableFilter
 
                     // events
-                    onGridReady={this.onGridReady}>
+                    onGridReady={this.onGridReady}
+                    >
                 </AgGridReact>
             </div>
         )
     }
-};
+}
+
+function formatNumber(params) {
+    let num = parseFloat(Math.round(params.value * 100) / 100).toFixed(2);
+    if(isNaN(num)) {
+      return "-";
+    }
+    else return num;
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,125 @@
 'use strict';
 
 import React from "react";
-import {render} from "react-dom";
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Modal from 'react-modal';
+
 
 // pull in the ag-grid styles we're interested in
 import "ag-grid-root/dist/styles/ag-grid.css";
 import "ag-grid-root/dist/styles/theme-fresh.css";
 
-// only necessary if you're using ag-Grid-Enterprise features
-// import "ag-grid-enterprise";
-
 // our application
 import PlayerGrid from "./PlayerGrid";
 
-document.addEventListener('DOMContentLoaded', () => {
-    render(
-        <PlayerGrid/>,
-        document.querySelector('#app')
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      modalIsOpen: false,
+      startingBudget: 1980,
+      rowData: []
+    }
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.onPlayerPurchase = this.onPlayerPurchase.bind(this);
+
+    axios.get(`http://localhost:4000/players`)
+    .then(res => {
+      this.setState({
+        rowData: calcInflation(mergeSavedPrices(res.data), this.state.startingBudget),
+      });
+    });
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  onPlayerPurchase() {
+    this.setState({
+      rowData: calcInflation(this.state.rowData, this.state.startingBudget)
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.openModal}>Open Modal</button>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+
+          <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+          <button onClick={this.closeModal}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </Modal>
+        <PlayerGrid
+          rowData={this.state.rowData}
+          onPlayerPurchase={this.onPlayerPurchase}>
+        </PlayerGrid>
+      </div>
     );
-});
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
+function calcInflation(players, startingBudget) {
+  let accumulatedValue = 0;
+  players.forEach((player) => {
+    if(player.hasOwnProperty('purchase_price') && !isNaN(player.purchase_price)) {
+      accumulatedValue += player.base_price - player.purchase_price;
+    }
+  });
+  let inflationRate = (startingBudget + accumulatedValue) / startingBudget
+  players.forEach((player) => {
+    player.inflated_price = inflationRate * player.base_price;
+  });
+  return players;
+}
+
+function mergeSavedPrices(players) {
+  players.forEach((player) => {
+    let purchasePrice = localStorage.getItem(player.player_id + ".purchase_price");
+    if(purchasePrice) {
+      player.purchase_price = purchasePrice;
+    }
+    purchasePrice = null;
+  });
+  return players;
+}
